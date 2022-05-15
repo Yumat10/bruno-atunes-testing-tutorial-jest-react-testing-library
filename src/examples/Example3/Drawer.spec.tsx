f8dac36e264abf9b8214d38c5d08aa38180bafb6
Yumat <mr.yuma.tanaka@gmail.com>
2022-05-14 22:30:42 -0400

    Added tests for mocking

--- src/examples/Example3/Drawer.spec.tsx
+++ src/examples/Example3/Drawer.spec.tsx
@@ -9,19 +9,20 @@ import { MyDrawer } from './Drawer';
 import user from '@testing-library/user-event';
 
 // use jest.mock factory and show require actual
+jest.mock('@material-ui/core', () => ({
+  ...jest.requireActual('@material-ui/core'),
+  SwipeableDrawer: jest.fn(() => <div>Hello World!</div>),
+}));
 
 describe('Drawer', () => {
-  it('shows no "Hello YouTube!"', () => {
+  it('shows no "Hello World!"', () => {
     render(<MyDrawer />);
-    expect(screen.queryByText('Hello YouTube!')).not.toBeInTheDocument();
+    expect(screen.queryByText('Hello World!')).toBeInTheDocument();
   });
 
-  it('clicking on "Open Drawer" Button shows "Hello YouTube!"', async () => {
+  it('clicking on "Open Drawer" Button shows "Hello World!"', () => {
     render(<MyDrawer />);
     user.click(screen.getByRole('button', { name: 'Open Drawer' }));
-    expect(screen.getByText('Hello YouTube!')).toBeInTheDocument();
-
-    user.keyboard('{escape}');
-    await waitForElementToBeRemoved(() => screen.getByText('Hello YouTube!'));
+    expect(screen.getByText('Hello World!')).toBeInTheDocument();
   });
 });
